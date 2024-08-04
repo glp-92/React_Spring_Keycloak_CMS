d856251f7cd631be7d2625a8a3984b4aa3f4d5e3
glp-92 <guillermolopezpazos@gmail.com>
2024-08-04 12:03:05 +0200

    politica de privacidad en impersonal

--- frontend/clientapp/src/pages/privacyPolicy/privacyPolicy.jsx
+++ frontend/clientapp/src/pages/privacyPolicy/privacyPolicy.jsx
@@ -13,13 +13,13 @@ const PrivacyPolicy = () => {
 
             <Box mt={4}>
                 <Typography variant="h2" gutterBottom>
-                    1. Información que Recopilamos
+                    1. Información Recopilada
                 </Typography>
                 <Typography variant="body1" paragraph>
-                    No recopilamos información personal de nuestros visitantes a menos que nos la proporcionen voluntariamente. La única información que recopilamos automáticamente incluye:
+                    No se recopila información personal de visitantes a menos que sea proporcionada voluntariamente, exceptuando el siguiente caso:
                 </Typography>
                 <Typography variant="body1" component="ul">
-                    <li>Datos de análisis básicos: Información sobre la ubicación general (país o región) desde la que acceden los usuarios a nuestro sitio, el número de visitas y el número de páginas vistas.</li>
+                    <li>Datos de análisis básicos: Información sobre la ubicación general (país o región) desde la que acceden los usuarios al sitio, el número de visitas y el número de páginas vistas.</li>
                 </Typography>
             </Box>
 
@@ -31,10 +31,10 @@ const PrivacyPolicy = () => {
                     La información recopilada se utiliza exclusivamente para:
                 </Typography>
                 <Typography variant="body1" component="ul">
-                    <li>Analizar el tráfico y el comportamiento de los usuarios en el sitio para mejorar el contenido y la experiencia del usuario.</li>
+                    <li>Analizar el tráfico y el comportamiento de los usuarios en el sitio para mejorar el contenido y la experiencia de usuario.</li>
                 </Typography>
-                <Typography variant="body1" paragraph>
-                    No compartimos, vendemos ni alquilamos ninguna información a terceros.
+                <Typography marginTop={1} variant="body1" paragraph>
+                    No se comparte ni vende información a terceros.
                 </Typography>
             </Box>
 
@@ -43,10 +43,10 @@ const PrivacyPolicy = () => {
                     3. Cookies
                 </Typography>
                 <Typography variant="body1" paragraph>
-                    Nuestro sitio utiliza cookies mínimas para realizar un seguimiento del tráfico y de los patrones de uso. Estas cookies no contienen ninguna información personal identificable.
+                    El sitio web utiliza cookies mínimas para realizar un seguimiento del tráfico y de los patrones de uso. Estas cookies no contienen ninguna información personal identificable.
                 </Typography>
                 <Typography variant="body1" paragraph>
-                    Puedes desactivar el uso de cookies modificando la configuración de tu navegador, aunque esto puede afectar a la funcionalidad del sitio.
+                    Se puede desactivar el uso de cookies modificando la configuración del navegador, aunque esto puede afectar a la funcionalidad del sitio.
                 </Typography>
             </Box>
 
@@ -55,7 +55,7 @@ const PrivacyPolicy = () => {
                     4. Seguridad
                 </Typography>
                 <Typography variant="body1" paragraph>
-                    Nos comprometemos a proteger la información recopilada de cualquier acceso no autorizado, alteración o destrucción. Sin embargo, ten en cuenta que ninguna transmisión por Internet es completamente segura.
+                    Se protege la información recopilada de cualquier acceso no autorizado, alteración o destrucción. Sin embargo, ninguna transmisión por Internet es completamente segura.
                 </Typography>
             </Box>
 
@@ -64,7 +64,7 @@ const PrivacyPolicy = () => {
                     5. Enlaces a Terceros
                 </Typography>
                 <Typography variant="body1" paragraph>
-                    Nuestro sitio puede contener enlaces a otros sitios web. No nos hacemos responsables del contenido ni de las prácticas de privacidad de estos sitios de terceros. Te recomendamos leer las políticas de privacidad de cualquier sitio web externo que visites.
+                    El sitio web puede contener enlaces a otros sitios. No se asume responsabilidad del contenido ni de las prácticas de privacidad de estos sitios de terceros. Se recomienda leer las políticas de privacidad de cualquier sitio web externo visitado.
                 </Typography>
             </Box>
 
@@ -73,7 +73,7 @@ const PrivacyPolicy = () => {
                     6. Cambios en la Política de Privacidad
                 </Typography>
                 <Typography variant="body1" paragraph>
-                    Podemos actualizar esta Política de Privacidad de vez en cuando. Cualquier cambio será publicado en esta página con la fecha de actualización revisada. Te recomendamos revisar esta política periódicamente.
+                    La política de privacidad está sujeta a cambios. Cualquier cambio será publicado en esta página con la fecha de actualización revisada.
                 </Typography>
             </Box>
 
@@ -82,7 +82,7 @@ const PrivacyPolicy = () => {
                     7. Contacto
                 </Typography>
                 <Typography variant="body1" paragraph>
-                    Si tienes alguna pregunta sobre esta Política de Privacidad, puedes ponerte en contacto con nosotros a través de <Link href="mailto:tucorreo@ejemplo.com">tucorreo@ejemplo.com</Link>.
+                    Ante cualquier duda, el contacto es <Link href="mailto:tucorreo@ejemplo.com">tucorreo@ejemplo.com</Link>.
                 </Typography>
             </Box>
         </Container>
